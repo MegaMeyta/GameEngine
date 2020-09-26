6a54c628412e19730a949abf5b88f968d76d8711
Joshua Metz <joshuametz033@gmail.com>
2020-09-26 12:14:46 -0700

    made some variables global

--- GamingTime/global.h
+++ GamingTime/global.h
@@ -5,8 +5,8 @@
 #include <string.h>
 #include <iostream>
 
-float desktopWidth = sf::VideoMode::getDesktopMode().width / 2;
-float desktopHeight = sf::VideoMode::getDesktopMode().height / 2;
+float desktopWidth = sf::VideoMode::getDesktopMode().width / 1;
+float desktopHeight = sf::VideoMode::getDesktopMode().height / 1;
 float gameWidth = desktopWidth;
 float gameHeight = desktopHeight;
-sf::RenderWindow window(sf::VideoMode(desktopWidth, desktopHeight), "SFML works!", sf::Style::Titlebar || sf::Style::Close);
+sf::RenderWindow window(sf::VideoMode(desktopWidth, desktopHeight), "SFML works!", sf::Style::Close);
